928f23e6c4437e7ad69ce4449b114d9325a61c49
KycToViK <magaaaaa666@mail.ru>
2024-12-23 15:29:30 +0300

    made

---
+++ test/BankAccountTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class BankAccountTest {
+
+    @Test
+    public void shouldNotBeBlockedWhenCreated() {
+        BankAccount account = new BankAccount("a", "b");
+        assertFalse(account.isBlocked());
+    }
+
+    @Test
+    public void shouldReturnZeroAmountAfterActivation() {
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("RUB");
+        assertEquals(Integer.valueOf(0), account.getAmount());
+        assertEquals("RUB", account.getCurrency());
+    }
+
+    @Test
+    public void shouldReturnFirstNameThenSecondName() {
+        BankAccount account = new BankAccount("a","b");
+        String[] a = account.getFullName();
+        String[] b = {"a","b"};
+        Assertions.assertArrayEquals(b,a);
+    }
+}
